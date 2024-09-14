0907dfd227a1f054d67365719ef342d5308f1ee7
Tiago Lopes <saxtiago14@gmail.com>
2024-09-14 20:10:23 -0300

    feat: project for insert data using k6 library

---
+++ Customers.Load/Load/PostCustomers.js
@@ -0,0 +1,36 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { SharedArray } from 'k6/data';
+import execution from 'k6/execution';
+
+
+const data = new SharedArray("Read Json file", () => {
+  return JSON.parse(open('./data.json'))
+})
+
+export const options = {
+  vus: 1,
+  duration: '2m',
+  iterations: data.length
+};
+
+export default function () {
+
+  const indexCurrent = execution.scenario.iterationInInstance;
+
+  console.log(`Indice da interação atual: ${indexCurrent} || Cenário: ${execution.scenario.name}`)
+
+  const url = 'http://localhost:5048/api/Customer/CreateCustomer';
+
+  const payload = JSON.stringify(data[indexCurrent]);
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      "Accept": "application/json"
+    },
+  };
+
+  http.post(url, payload, params);
+  sleep(1);
+}
